865d6389db6bda31bf5e8c8405bc5baaff9de9d9
Shadi Yadollahi Khales <syadollahikhales@gmail.com>
2022-01-28 19:21:27 -0500

    Remove unneccessary code

--- src/main/java/frc/robot/IO.java
+++ src/main/java/frc/robot/IO.java
@@ -1,15 +1,10 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 public class IO {
 
     XboxController driverController = new XboxController(0);
-    JoystickButton driverXboxAButton = new JoystickButton(driverController, 1);
-    JoystickButton driverXboxBButton = new JoystickButton(driverController, 2);
-    JoystickButton driverXboxYButton = new JoystickButton(driverController, 3);
-    JoystickButton driverXboxXButton = new JoystickButton(driverController, 4);
 
     //TODO
     static final double joystickDeadzone = 0.15;
